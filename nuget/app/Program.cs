f5d293d11430de7c085112d16b58c42017d30d39
pochernina <olya_pochernina@mail.ru>
2022-09-20 11:56:50 +0300

    project without tests and async

---
+++ nuget/app/Program.cs
@@ -0,0 +1,8 @@
+﻿using NuGetNN;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+Image<Rgb24> image = Image.Load<Rgb24>("face1.png");
+System.Collections.Generic.IEnumerable<(string Emotion, float Prob)> emotions = EmotionNN.EmotionFerplus(image);
+foreach (var i in emotions)
+    Console.WriteLine($"{i.Emotion}: {i.Prob}");
---
+++ nuget/package/EmotionNN.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+namespace NuGetNN
+{
+    public static class EmotionNN
+    {
+        public static Array EmotionFerplus(Image<Rgb24> image)
+        {
+            using var modelStream = typeof(EmotionNN).Assembly.GetManifestResourceStream("package.emotion-ferplus-7.onnx");
+            using var memoryStream = new MemoryStream();
+            modelStream!.CopyTo(memoryStream);
+            using var session = new InferenceSession(memoryStream.ToArray());
+
+            image.Mutate(ctx => ctx.Resize(new Size(64, 64)));
+
+            var inputs = new List<NamedOnnxValue> { NamedOnnxValue.CreateFromTensor("Input3", GrayscaleImageToTensor(image)) };
+            using IDisposableReadOnlyCollection<DisposableNamedOnnxValue> results = session.Run(inputs);
+            var emotions = Softmax(results.First(v => v.Name == "Plus692_Output_0").AsEnumerable<float>().ToArray());
+
+            string[] keys = { "neutral", "happiness", "surprise", "sadness", "anger", "disgust", "fear", "contempt" };
+            return keys.Zip(emotions).ToArray();
+        }
+
+        private static DenseTensor<float> GrayscaleImageToTensor(Image<Rgb24> img)
+        {
+            var w = img.Width;
+            var h = img.Height;
+            var t = new DenseTensor<float>(new[] { 1, 1, h, w });
+
+            img.ProcessPixelRows(pa =>
+            {
+                for (int y = 0; y < h; y++)
+                {
+                    Span<Rgb24> pixelSpan = pa.GetRowSpan(y);
+                    for (int x = 0; x < w; x++)
+                    {
+                        t[0, 0, y, x] = pixelSpan[x].R; // B and G are the same
+                    }
+                }
+            });
+
+            return t;
+        }
+        private static float[] Softmax(float[] z)
+        {
+            var exps = z.Select(x => Math.Exp(x)).ToArray();
+            var sum = exps.Sum();
+            return exps.Select(x => (float)(x / sum)).ToArray();
+        }
+    }
+}
