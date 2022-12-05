234de828392c7e4b067cb3bdaacd7ce9e82364a8
pochernina <olya_pochernina@mail.ru>
2022-12-05 14:30:43 +0300

    task3

---
+++ wpf_with_db/wpf_with_db/Migrations/20221205070715_first_variant.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace wpfwithdb.Migrations
+{
+    /// <inheritdoc />
+    public partial class firstvariant : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Hash",
+                table: "Items",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Hash",
+                table: "Items",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+    }
+}
